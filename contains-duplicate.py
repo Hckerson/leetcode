2687af94589b9428b7edde158031f99fc1a1c575
hckerson <hckerson@gmail.com>
2026-09-05 16:27:37 +0100

    Remove outdated documents: deleted resume-source.html, stipend-timetable.pdf, and who-actually-sponsors-interns.pdf to streamline project documentation and focus on current resources.

--- contains-duplicate.py
+++ contains-duplicate.py
@@ -3,11 +3,7 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        if len(nums) <= 2:
-            if len == 1:
-                return False
-            if len == 2:
-                return nums[0] == nums[1]
+
         store = {}
 
         for i in range(len(nums)):
