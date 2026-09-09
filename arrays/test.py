277e92d48f88502cb26af451b6a4fd27e312f383
hckerson <hckerson@gmail.com>
2026-09-09 01:52:37 +0100

    Remove test.py and add bitwise-trailing-zero.py with hasTrailingZeros implementation

--- arrays/test.py
+++
@@ -1,3 +0,0 @@
-key = "uh4nk3j4"
-total = sum(ord(char) for char in key)
-print(total)
---
+++ hash/bitwise-trailing-zero.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def hasTrailingZeros(self, nums: List[int]) -> bool:
+        count = 0
+        for num in nums:
+            if num % 2 == 0:
+                count += 1
+
+        return count > 1
