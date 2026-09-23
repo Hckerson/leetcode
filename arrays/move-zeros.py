28186fa47839e29e874107c85bfa54ad00bfb038
Hckerson <166761968+Hckerson@users.noreply.github.com>
2026-09-23 15:14:09 +0100

    Refactor moveZeroes implementation and add removeDuplicates function

--- arrays/move-zeros.py
+++ arrays/move-zeros.py
@@ -8,15 +8,6 @@ Note that you must do this in-place without making a copy of the array.
 
 
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        insert = 0
-        for num in nums:
-            if num != 0:
-                nums[insert] = num
-                insert += 1
-
-        for i in range(insert, len(nums)):
-            nums[i] = 0
 
     def moveZeroesSwap(self, nums: List[int]) -> None:
         insert = 0
--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-x = [0, 1, 0, 3, 12]
-[1, 1, 0, 3, 12]
-[1, 3, 0, 3, 12]
-[1, 3, 12, 3, 12]
-count = 0
-y = [num for num in x if count_and_increment(num)]
+x = [0, 0, 1, 1, 1, 2, 2]
+12
+4
+[1, 1, 2, 2, 3, 12]
+[1, 2, 1, 2, 3, 12]
+[1, 2, 3, 2, 1, 12]
+[1, 2, 3, 12, 1, 2]
 
 
-def count_and_increment(num):
-    if num > 0:
-        return True
-    count += 1
-    return False
+def p_x(num):
+    num = list(set(num))
+    print(num)
 
 
-print()
+p_x(x)
+print(x)
