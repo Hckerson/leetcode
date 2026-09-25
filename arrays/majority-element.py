eb5638531e643ca09e00e4c64385593d03586241
Hckerson <166761968+Hckerson@users.noreply.github.com>
2026-09-25 22:26:01 +0100

    Refactor majorityElement and max_sum_using_slider implementations; update test data in test.py

--- arrays/majority-element.py
+++ arrays/majority-element.py
@@ -1,6 +1,7 @@
 from typing import List
+from collections import Counter
 
-x = [1,2,3, 1]
+x = [1, 2, 3, 1]
 
 """
 Given an array nums of size n, return the majority element.
@@ -8,16 +9,15 @@ Given an array nums of size n, return the majority element.
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 """
 
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        store = {}
-        freq = (nums[0], 0)
+        candidate, count = nums[0], 0
 
         for num in nums:
-            new_count = store.get(num, 0) + 1
-            store[num] = new_count
+            if count == 0:
+                candidate = num
 
-            if new_count > freq[1]:
-                freq = (num, new_count)
+            count +=1 if num == candidate else -1
 
-        return freq[0]
+        return candidate
--- arrays/max-sum.py
+++ arrays/max-sum.py
@@ -5,39 +5,23 @@ Learnt Sliding window technique here
 
 """
 
+
 class Solution:
-
-    def max_sum(self, arr, n, k):
-        max_count = -sys.maxsize
-
-        for i in range(n - k + 1):
-            current_sum = 0
-            for j in range(k):
-                current_sum += arr[i + j]
-
-            max_count = max(max_count, current_sum)
-
-        return max_count
-
     def max_sum_using_slider(self, arr, n, k):
         if n < k:
             return -1
 
-        window_sum = sum(arr[:k])
-
-        max_sum = window_sum
+        window_count = sum(arr[:k])
 
         for i in range(n - k):
-            window_sum = window_sum - arr[i] + arr[i + k]
-            max_sum = max(max_sum, window_sum)
+            recomputed_value = window_count - arr[i] + arr[i + k]
+            window_count = max(window_count, recomputed_value)
 
-        return max_sum
+        return window_count
 
 
 arr = [5, 2, -1, 0, 3]
 k = 3
 n = len(arr)
-x = Solution().max_sum(arr, n, k)
 y = Solution().max_sum_using_slider(arr, n, k)
-print(x)
 print(y)
